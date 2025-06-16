13a94589e6bdf085c1578bcc9ccf996ea4dda5ea
youcef <youcefabdellani@gmail.com>
2025-06-16 15:11:03 +0100

    add the reservation form page

--- FreeCodeCamp_crash_course/Django_Crash_Course/firstapp/views.py
+++ FreeCodeCamp_crash_course/Django_Crash_Course/firstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views import View
+from .forms import ReservationForm
 
 # Create your views here.
 def hello_world(request):
@@ -9,3 +10,14 @@ def hello_world(request):
 class HelloAlgeria(View):
   def get(self, request):
     return HttpResponse("Hello Algeria")
+
+def home(request):
+  form = ReservationForm()
+
+  if request.method == 'POST':
+    form = ReservationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return HttpResponse('Success')
+  return render(request, 'index.html', {'form': form})
+
