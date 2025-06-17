823db5fbf7ce2491f64db1255a94801f5007b012
youcef <youcefabdellani@gmail.com>
2025-06-17 14:58:20 +0100

    create the core app and install the python_dotenv and psycoppg packages to the app

--- Online_Marketplace/OnlineMarket/OnlineMarket/settings.py
+++ Online_Marketplace/OnlineMarket/OnlineMarket/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +29,11 @@ SECRET_KEY = 'django-insecure-7d#s1tn*ebazthehzj91&vn$g_=m5v1x1j-%5*n3d64#y48p4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '192.168.1.7',  # e.g., '192.168.1.100'
+]
 
 
 # Application definition
@@ -37,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
