e3ab25d93abc54b38316df71b639e71d0b5a1ffb
youcef <youcefabdellani@gmail.com>
2025-06-19 18:01:44 +0100

    add the backend login for creating the Itels

--- Online_Marketplace/OnlineMarket/item/urls.py
+++ Online_Marketplace/OnlineMarket/item/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'item'
+
 urlpatterns = [
   path('detail/<int:pk>', views.detail, name='item_detail'),
+  path('new/', views.new, name='item_new'),
 ]
