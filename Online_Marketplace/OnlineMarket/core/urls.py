d82646f5fc8712fe303f30babfc86abb700e6de3
youcef <youcefabdellani@gmail.com>
2025-06-19 18:00:45 +0100

    add the name of the app to the Core urls

--- Online_Marketplace/OnlineMarket/core/urls.py
+++ Online_Marketplace/OnlineMarket/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
 from .forms import LoginForm
+
+app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='home'),
