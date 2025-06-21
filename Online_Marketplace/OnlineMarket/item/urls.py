8f5be0a039dc9b4d3d54ac078d002d54018b6313
youcef <youcefabdellani@gmail.com>
2025-06-21 10:39:35 +0100

    add the item delete functionality

--- Online_Marketplace/OnlineMarket/item/urls.py
+++ Online_Marketplace/OnlineMarket/item/urls.py
@@ -6,4 +6,5 @@ app_name = 'item'
 urlpatterns = [
   path('detail/<int:pk>', views.detail, name='item_detail'),
   path('new/', views.new, name='item_new'),
+  path('<int:pk>/delete',views.delete, name='delete')
 ]
