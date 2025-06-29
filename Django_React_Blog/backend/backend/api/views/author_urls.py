af91420cfe8ab44311af787204164d3e79b13801
youcef <youcefabdellani@gmail.com>
2025-06-29 19:03:11 +0100

    add the author urls to the API

---
+++ Django_React_Blog/backend/backend/api/views/author_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import dashboards
+
+urlpatterns = [
+  path('stats/<user_id>', dashboards.DashboardStatsView.as_view()),
+  path('post-list/<user_id>', dashboards.DashboardPostListsView.as_view()),
+  path('comment-list', dashboards.DashboardCommentListView.as_view()),
+  path('noti-list/<user_id>/',dashboards.DashboardNotificationsListView.as_view()),
+  path('noti-mark-seen/', dashboards.DashboardMarkNotiSeenAPIView.as_view()),
+  path('reply-comment', dashboards.DashboardReplyCommentAPIView.as_view())
+]
