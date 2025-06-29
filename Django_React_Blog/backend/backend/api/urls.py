5f565d5dbfffdc961b4c606ce481bdd2ac147747
youcef <youcefabdellani@gmail.com>
2025-06-29 09:53:55 +0100

    add the bookmark endpoint to the api

--- Django_React_Blog/backend/backend/api/urls.py
+++ Django_React_Blog/backend/backend/api/urls.py
@@ -14,5 +14,6 @@ urlpatterns = [
   path('post/list/', api_view.PostListAPIView.as_view()),
   path('post/detail/<slug>/', api_view.PostDetailAPIView.as_view()),
   path('post/post-like/', api_view.LikePostAPIView.as_view()),
-  path('post/comment-post/', api_view.PostCommentAPIView.as_view())
+  path('post/comment-post/', api_view.PostCommentAPIView.as_view()),
+  path('post/bookmark-post/', api_view.PostBookmarkAPIView.as_view())
 ]
