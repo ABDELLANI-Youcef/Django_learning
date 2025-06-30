59d01e53fcb69832f4f91c5828c2eb70e2589f47
youcef <youcefabdellani@gmail.com>
2025-06-30 15:58:21 +0100

    install the package drf_spectacular and add the swagger in the endpoint

--- Django_React_Blog/backend/backend/backend/urls.py
+++ Django_React_Blog/backend/backend/backend/urls.py
@@ -17,27 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 from django.conf import settings
 from django.conf.urls.static import static
 
-schema_view = get_schema_view(
-  openapi.Info(
-    title="Blog Backend APIs",
-    default_version="v1",
-    description="This is the documentation for the backend API",
-    terms_of_service="http://mywbsite.com/policies/",
-    contact=openapi.Contact(email="youcefabdellani@gmail.com"),
-    license=openapi.License(name="MIT Licence"),
-  ),
-  public=True,
-  permission_classes = (permissions.AllowAny, )
-)
-
 urlpatterns = [
   path('admin/', admin.site.urls),
   path('api/v1/', include('api.urls')),
-  path("", schema_view.with_ui('swagger', cache_timeout=0), name="schema-swagger-ui")
+  path('schema/', SpectacularAPIView.as_view(), name='schema'),
+  path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
