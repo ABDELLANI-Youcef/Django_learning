4aff0de93b5d5657d5205d5ef44e9a5152da4088
youcef <youcefabdellani@gmail.com>
2025-07-13 15:48:45 +0100

    add the bookmark logic to the Details interface

--- Django_React_Blog/frontend/src/views/core/Detail.jsx
+++ Django_React_Blog/frontend/src/views/core/Detail.jsx
@@ -33,8 +33,6 @@ function Detail() {
     )
   }
 
-  fetchPost()
-
   const handleCreateCommentSubmit = async (event) => {
     event.preventDefault()
 
@@ -55,6 +53,29 @@ function Detail() {
         comment: ""
       }
     )
+    fetchPost()
+  }
+
+  const handleLikePost = async () => {
+    const json = {
+      user_id: 4,
+      post_id: post?.id
+    }
+    const response = await apiInstance.post('post/post-like/', json)
+    console.log(response.data);
+    Toast("success", "Post liked")
+    fetchPost()
+  }
+  const handleBookmarkPost = async () => {
+    const json = {
+      user_id: 4,
+      post_id: post?.id
+    }
+
+    const response = await apiInstance.post('post/bookmark-post/', json)
+    console.log(response.data);
+    Toast("success", "Post bookmarked")
+    fetchPost()
   }
 
   return (
@@ -142,6 +163,12 @@ function Detail() {
                     </li>))
                   }
                 </ul>
+                <button className='btn btn-primary' onClick={handleLikePost}>
+                  <i className="fas fa-thumbs-up me-2"></i>{post?.likes?.length}
+                </button>
+                <button className='btn btn-danger ms-2' onClick={handleBookmarkPost}>
+                  <i className="fas fa-bookmark"></i>
+                </button>
               </div>
             </div>
             {/* Left sidebar END */}
